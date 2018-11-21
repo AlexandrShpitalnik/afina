7cd44a2f2000abef11bf0eba25725a521c078c61
AlexandrShpitalnik <alexandr_shpitalnik@mail.ru>
2018-11-21 21:03:09 +0300

    hw coroutine(some bugs remains)

---
+++ src/network/st_nonblocking/Connection.cpp
@@ -0,0 +1,140 @@
+#include "Connection.h"
+
+#include <iostream>
+#include <sys/uio.h>
+#include <spdlog/logger.h>
+#include <sys/socket.h>
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+#include <afina/logging/Service.h>
+
+#include "protocol/Parser.h"
+
+
+namespace Afina {
+namespace Network {
+namespace STnonblock {
+
+// See Connection.h
+void Connection::Start(std::shared_ptr<spdlog::logger> logger) {
+    std::cout << "Start" << std::endl;
+    readed_bytes = -1;
+    _event.events = EPOLLIN|EPOLLRDHUP|EPOLLERR;
+    _event.data.fd = _socket;
+    _event.data.ptr = this;
+    _is_running = true;
+    _logger = logger;
+}
+
+// See Connection.h
+void Connection::OnError() {
+    std::cout << "OnError" << std::endl;
+   // shutdown(_socket, SHUT_RDWR);
+    _is_running = false;
+}
+
+// See Connection.h
+void Connection::OnClose() {
+    std::cout << "OnClose" << std::endl;
+   // shutdown(_socket, SHUT_RDWR);
+    _is_running = false;
+}
+
+// See Connection.h
+void Connection::DoRead() {
+    std::cout << "DoRead" << std::endl;
+    std::string result;
+    while (isAlive() && (readed_bytes = read(_socket, client_buffer, sizeof(client_buffer))) > 0) {
+        _logger->debug("Got {} bytes from socket", readed_bytes);
+        while (readed_bytes > 0 && isAlive()) {
+            _logger->debug("Process {} bytes", readed_bytes);
+            // There is no command yet
+            if (!command_to_execute) {
+                std::size_t parsed = 0;
+                if (parser.Parse(client_buffer, readed_bytes, parsed)) {
+                    _logger->debug("Found new command: {} in {} bytes", parser.Name(), parsed);
+                    command_to_execute = parser.Build(arg_remains);
+                    if (arg_remains > 0) {
+                        arg_remains += 2;
+                    }
+                }
+                if (parsed == 0) {
+                    break;
+                } else {
+                    std::memmove(client_buffer, client_buffer + parsed, readed_bytes - parsed);
+                    readed_bytes -= parsed;
+                }
+            }
+
+            // There is command, but we still wait for argument to arrive...
+            if (command_to_execute && arg_remains > 0) {
+                _logger->debug("Fill argument: {} bytes of {}", readed_bytes, arg_remains);
+                // There is some parsed command, and now we are reading argument
+                std::size_t to_read = std::min(arg_remains, std::size_t(readed_bytes));
+                argument_for_command.append(client_buffer, to_read);
+
+                std::memmove(client_buffer, client_buffer + to_read, readed_bytes - to_read);
+                arg_remains -= to_read;
+                readed_bytes -= to_read;
+            }
+
+            // Thre is command & argument - RUN!
+            if (command_to_execute && arg_remains == 0) {
+                _logger->debug("Start command execution");
+                if(argument_for_command.back() == '\n'){
+                    argument_for_command.pop_back();
+                    argument_for_command.pop_back();
+                }
+                command_to_execute->Execute(*_pStorage, argument_for_command, result);
+                result += "\r\n";
+                global_result.push_back(result);
+
+                SetWrite();
+                command_to_execute.reset();
+                argument_for_command.resize(0);
+                parser.Reset();
+            }
+        }
+    }
+}
+
+// See Connection.h
+void Connection::DoWrite() {
+    std::cout << "DoWrite" << std::endl;
+    auto iov = new struct iovec[global_result.size()];
+    int written;
+    iov[0].iov_base =&(global_result[0].front()) + offs;
+    iov[0].iov_len = global_result[0].size() - offs;
+    for (int i = 1; i < global_result.size(); i++){
+        iov[i].iov_base =&(global_result[i].front());
+        iov[i].iov_len = global_result[i].size();
+    }
+
+    if ((written = writev(_socket, iov, global_result.size())) <= 0) {
+        throw std::runtime_error("Failed to send response");
+    }
+
+
+    for (int i = 0; i < global_result.size() && written; i++) {
+        if (written >= iov[i].iov_len) {
+            written -= iov[i].iov_len;
+            global_result.pop_front();
+        } else {
+            offs = written;
+        }
+    }
+
+    delete[] iov;
+    _event.events = EPOLLIN|EPOLLRDHUP|EPOLLERR;
+    if (offs != 0){
+        SetWrite();
+    }
+}
+
+void Connection::SetWrite() {
+    _event.events |= EPOLLOUT;
+}
+
+} // namespace STnonblock
+} // namespace Network
+} // namespace Afina
---
+++ src/network/st_nonblocking/Connection.h
@@ -0,0 +1,59 @@
+#ifndef AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
+#define AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
+
+#include <cstring>
+#include <deque>
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
+#include <afina/logging/Service.h>
+#include "protocol/Parser.h"
+#include <sys/epoll.h>
+
+namespace Afina {
+namespace Network {
+namespace STnonblock {
+
+class Connection {
+public:
+    Connection(int s, std::shared_ptr<Afina::Storage> ps) : _socket(s), _pStorage(ps), offs(0) {
+        std::memset(&_event, 0, sizeof(struct epoll_event));
+       //_event.data.ptr = this;
+    }
+
+    inline bool isAlive() const { return _is_running; }
+
+    void Start(std::shared_ptr<spdlog::logger> logger);
+
+protected:
+    void OnError();
+    void OnClose();
+    void DoRead();
+    void DoWrite();
+
+private:
+    friend class ServerImpl;
+
+    void SetWrite();
+
+    std::shared_ptr<spdlog::logger> _logger;
+    int _socket;
+    struct epoll_event _event;
+    bool _is_running;
+    int readed_bytes;
+    char client_buffer[4096];
+    std::size_t arg_remains;
+    Protocol::Parser parser;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    std::string argument_for_command;
+    std::deque<std::string> global_result;
+    size_t offs;
+    //std::string global_result;
+    std::shared_ptr<Afina::Storage> _pStorage;
+
+};
+
+} // namespace STnonblock
+} // namespace Network
+} // namespace Afina
+
+#endif // AFINA_NETWORK_ST_NONBLOCKING_CONNECTION_H
